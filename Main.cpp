a994e2d76dd31841431be078b323d0fa868bed18
HongSee <bjh0820@naver.com>
2021-05-28 06:58:49 +0900

    3차

--- Main.cpp
+++ Main.cpp
@@ -2,52 +2,15 @@
 
 int main()
 {
-    int sel;
-    srand((unsigned int)time(NULL)); //시드값 설정해주기.
-    string menu[8] = { "중식","일식","고기","양식","한식","치킨","분식/야식","기타" };
-    cout << "오늘 뭐 먹지..." << endl;
-    for (int i = 0; i < 8; i++)
-        cout << i + 1 << '.' << menu[i] << ' ';
-    cout << endl;
-    cin >> sel;
-    cout << menu[sel - 1] << "이(가) 땡긴다!" << endl;
+	int sel;
+	string menu[8] = { "중식","일식","고기","양식","한식","치킨","분식/야식","기타" };
+	srand((unsigned int)time(NULL)); //시드값 설정해주기.
 
-    Select(sel);
-    /*
-    Western west;
-    Etc etc;
+	cout << "오늘 뭐 먹지..." << endl;
+	cout << "1.중식 2.일식 3.고기 4.양식 5.한식 6.치킨 7.디저트 8.기타" << endl;
+	cin >> sel;
+	cout << menu[sel - 1] << "이(가) 땡긴다!" << endl;
 
-    switch (k) {
-    case 1:
-        cout << "중식이 선택됐습니다." << endl << endl;
-        //chinese chin;
-        //chin.ShowRandom();
-        break;
-    case 2:
-        cout << "일식이 선택됐습니다." << endl << endl;
-        //japanese jap;
-        break;
-    case 3:
-        cout << "고기가 선택됐습니다." << endl << endl;
-        break;
-    case 4:
-        cout << "양식이 선택됐습니다." << endl << endl;
-        west.ShowInfo();
-        break;
-    case 5:
-        cout << "한식이 선택됐습니다." << endl << endl;
-        break;
-    case 6:
-        cout << "치킨이 선택됐습니다." << endl << endl;
-        break;
-    case 7:
-        cout << "분식/야식이 선택됐습니다." << endl << endl;
-        break;
-    case 8:
-        cout << "기타가 선택됐습니다." << endl << endl;
-        etc.ShowInfo();
-        break;
-    }
-    */
+	Select(sel);
     return 0;
 }
--- Select.cpp
+++ Select.cpp
@@ -2,38 +2,27 @@
 
 void Select(int sel) 
 {    
-    Western west;
-    Etc etc;
+	Western west;
+	Etc etc;
 
-    switch (sel) {
-    case 1:
-        //cout << "중식이 선택됐습니다." << endl << endl;
-        //chinese chin;
-        //chin.ShowRandom();
-        break;
-    case 2:
-        //cout << "일식이 선택됐습니다." << endl << endl;
-        //japanese jap;
-        break;
-    case 3:
-        //cout << "고기가 선택됐습니다." << endl << endl;
-        break;
-    case 4:
-        //cout << "양식이 선택됐습니다." << endl << endl;
-        west.ShowInfo();
-        break;
-    case 5:
-        //cout << "한식이 선택됐습니다." << endl << endl;
-        break;
-    case 6:
-        //cout << "치킨이 선택됐습니다." << endl << endl;
-        break;
-    case 7:
-        //cout << "분식/야식이 선택됐습니다." << endl << endl;
-        break;
-    case 8:
-        //cout << "기타가 선택됐습니다." << endl << endl;
-        etc.ShowInfo();
-        break;
-    }
+	switch (sel) {
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		west.ShowInfo();
+		break;
+	case 5:
+		break;
+	case 6:
+		break;
+	case 7:
+		break;
+	case 8:
+		etc.ShowInfo();
+		break;
+	}
 }
